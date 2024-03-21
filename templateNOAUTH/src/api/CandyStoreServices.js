2aec4ec5145725cb33831601a3f09b625b4ae4ef
Angel Badillo <badilloa022402@gmail.com>
2024-03-20 20:52:00 -0500

    working on API calls

--- templateNOAUTH/src/api/CandyStoreServices.js
+++ templateNOAUTH/src/api/CandyStoreServices.js
@@ -3,14 +3,13 @@
  * and retreiving data for the candy store app.
  */
 
-
-export const api_url = "https://thehonoredone.live:8084/";
+const api_url = "https://thehonoredone.live:8084/";
 
 /**
  * Calls route for getting all categories.
  * @returns An array of categories.
  */
-export async function get_categories() {
+async function get_categories() {
     let path = "categories";
     let categories = [];
     try {
@@ -19,13 +18,13 @@ export async function get_categories() {
         const status = response.status;
         const success = status == 200;
         if (!success) {
-            throw new Error("${status} Operation Failed.")
+            throw new Error(`${status} Operation Failed.`)
         }
         categories = await json["categories"];
         return categories;
     }
     catch (error) {
-        throw new Error("Failed to get categories: ${error.message}");
+        throw new Error(`Failed to get categories: ${error.message}`);
     }
 }
 
@@ -35,7 +34,7 @@ export async function get_categories() {
  * corresponding to the /candies route.
  * @returns An array of candies.
  */
-export async function search_candies(candy_info = {}) {
+async function search_candies(candy_info = {}) {
     let path = "candies?" + new URLSearchParams(candy_info);
     let candies = [];
 
@@ -46,12 +45,12 @@ export async function search_candies(candy_info = {}) {
         const status = response.status;
         const success = status == 200;
         if (!success) {
-            throw new Error("${status} Operation Failed.");
+            throw new Error(`${status} Operation Failed.`);
         }
         return candies;
     }
     catch (error) {
-        throw new Error("Failed to get candies: ${error.message}");
+        throw new Error(`Failed to get candies: ${error.message}`);
     }
 }
 
@@ -59,17 +58,30 @@ export async function search_candies(candy_info = {}) {
  * Calls route for posting candy.
  * @param {*} candy_info An object representing a candy.
  */
-export async function post_candy(candy_info = {}) {
-    let path = "candies";
+async function post_candy(candy_info) {
+    let path = "candies?";
+
     try {
-        const response = await fetch(api_url + path, { method: "POST", body: JSON.stringify(candy_info) });
+        const response = await fetch(api_url + path, { method: "POST", body: JSON.stringify(candy_info)});
         const status = response.status;
         const success = status == 200;
-        if (!success) {
-            throw new Error("${status} Operation Failed.");
+        console.log(candy_info);
+        if (false) {
+            throw new Error(`${status} Operation Failed.`);
         }
+        return response.json();
     }
     catch (error) {
-        throw new Error("Failed to post candy: ${error.message}");
+        throw new Error(`Failed to post candy: ${error.message}`);
     }
 }
+
+const main = async ()=>{
+    let data = await search_candies({max_price: 0});
+    console.log(data);
+
+    data = await post_candy({id: 12345678, name: "Google", category: "Epic", category_id: 123456, prod_url: "None", img_url: "None", desc: "None", price: 1.0});
+    console.log(data);
+}
+
+main();
