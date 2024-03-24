c02238fa559d1701b3aa32899d5bb1159384f691
Angel Badillo <badilloa022402@gmail.com>
2024-03-23 19:22:07 -0500

    documentation

--- src/api/AwesomeStoreServices.js
+++ src/api/AwesomeStoreServices.js
@@ -29,25 +29,62 @@ async function get_categories() {
 }
 
 /**
- * Calls route for searching candies.
- * @param {*} item_info An object containing query params
- * corresponding to the /candies route.
- * @returns An array of candies.
+ * Performs a search using the given parameters, and returns the matching items.
+ * @param {string} id Item ID
+ * @param {string} name Item Name
+ * @param {string} prod_url URL for the item
+ * @param {string} img_url URL for the item's image
+ * @param {number} price Item price
+ * @param {string} desc Item description
+ * @param {number} skip Number of items to skip
+ * @param {number} limit Number of matching items to return
+ * @returns An array of items.
  */
-async function search_items(item_info = {}) {
-    let path = "items?" + new URLSearchParams(item_info);
-    let candies = [];
+async function search_items({ id, name, prod_url, img_url, price, desc, category, skip, limit } = {}) {
+    const query_params = {}
+
+    if (id !== null && id !== undefined)
+        query_params["id"] = id
+
+    if (name !== null && name !== undefined)
+        query_params["name"] = name
+
+    if (prod_url !== null && prod_url !== undefined)
+        query_params["prod_url"] = prod_url
+
+    if (img_url !== null && img_url !== undefined)
+        query_params["img_url"] = img_url
+
+    if (price !== null && price !== undefined)
+        query_params["price"] = price
+
+    if (desc !== null && desc !== undefined)
+        query_params["desc"] = desc
+
+    if (category !== null && category !== undefined)
+        query_params["category"] = category
+
+    if (skip !== null && skip !== undefined)
+        query_params["skip"] = skip
+
+    if (limit !== null && limit !== undefined)
+        query_params["limit"] = limit
+
+
+    let path = "items?" + new URLSearchParams(query_params);
+
+    let items = [];
 
     try {
         const response = await fetch(api_url + path, { method: "GET" });
         const json = await response.json();
-        candies = await json["candies"];
+        items = await json["items"];
         const status = response.status;
         const success = status == 200;
         if (!success) {
             throw new Error(`${status} Operation Failed.`);
         }
-        return candies;
+        return items;
     }
     catch (error) {
         throw new Error(`Failed to get candies: ${error.message}`);
@@ -55,33 +92,104 @@ async function search_items(item_info = {}) {
 }
 
 /**
- * Calls route for posting candy.
- * @param {*} item_info An object representing a candy.
+ * Calls route for posting item.
+ * @param {*} item An object representing a item.
  */
-async function post_item(item_info = {}) {
+async function post_item(item = {}) {
     let path = "items";
 
     try {
-        const response = await fetch(api_url + path, { method: "POST", body: JSON.stringify(item_info) });
+        const response = await fetch(api_url + path, {
+            method: "POST", headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(item)
+        });
         const status = response.status;
         const success = status == 200;
-        console.log(item_info);
-        console.log(await response.json());
         if (!success) {
             throw new Error(`${status} Operation Failed.`);
         }
-        return response.json();
+        return await response.json();
     }
     catch (error) {
-        throw new Error(`Failed to post candy: ${error.message}`);
+        throw new Error(`Failed to post item: ${error.message}`);
+    }
+}
+
+/**
+ * Calls route for putting /updating item.
+ * @param {*} item An object representing a item.
+ */
+async function put_item(id, item = {}) {
+    let path = "items/id/" + id;
+
+    try {
+        const response = await fetch(api_url + path, {
+            method: "PUT", headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }, body: JSON.stringify(item)
+        });
+        const status = response.status;
+        const success = status == 200;
+        if (!success) {
+            throw new Error(`${status} Operation Failed.`);
+        }
+        return await response.json();
+    }
+    catch (error) {
+        throw new Error(`Failed to put item: ${error.message}`);
+    }
+}
+
+/**
+ * 
+ * @param {*} id 
+ * @returns 
+ */
+async function delete_item(id) {
+    let path = "items/id/" + id;
+
+    try {
+        const response = await fetch(api_url + path, {
+            method: "DELETE", headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        const status = response.status;
+        const success = status == 200;
+        if (!success) {
+            throw new Error(`${status} Operation Failed.`);
+        }
+        return await response.json();
+    }
+    catch (error) {
+        throw new Error(`Failed to put item: ${error.message}`);
     }
 }
 
 const main = async () => {
-    let data = await search_items({ id: 42688339869883 });
+
+    let data = await get_categories();
+    console.log("GET /categories")
     console.log(data);
 
-    data = await post_item({ id: 12345, name: "Test", prod_url: "Stuff", img_url: "Stuff", price: 10.0, desc: "Hello here", category: "New", category_id: 123456 })
+    data = await search_items({ category: "Soft Candy", limit: 1 });
+    console.log("GET /items")
+    console.log(data);
+
+    data = await post_item({ name: "Test", prod_url: "Stuff", img_url: "Stuff", price: 10.0, desc: "Hello here", category: "Fortnite" })
+    console.log("POST /items")
+    console.log(data);
+
+    data = await put_item("65ff48a4aa724129f8404a1b", { name: "Test", prod_url: "Stuff", img_url: "Stuff", price: 10.0, desc: "Hello here", category: "New", category_id: "Fortnite" })
+    console.log("PUT /items/id")
+    console.log(data);
+
+    data = await delete_item("65ff48a4aa724129f8404a1b")
+    console.log("DELETE /items")
     console.log(data);
 }
 
