9866b730e47021eadcce604d2bbcd047fc77ab68
Angel Badillo <81447537+It-Is-Legend27@users.noreply.github.com>
2024-04-15 11:41:57 -0500

    working on AwesomeStoreServices.js, adding new functions for new API calls

--- src/api/AwesomeStoreServices.js
+++ src/api/AwesomeStoreServices.js
@@ -8,7 +8,7 @@ const api_url = "https://thehonoredone.live:8085/";
 /**
  * Represents an the information of an Item to be posted.
  */
-export class ItemInfo {
+class ItemInfo {
     /**
      * Creates a new instance of PostItem.
      * @param {string} name 
@@ -34,7 +34,7 @@ export class ItemInfo {
  * Represents an Item from the API call response. Includes ItemInfo as well 
  * as the ID of the item.
  */
-export class Item extends ItemInfo {
+class Item extends ItemInfo {
     /**
      * Creates a new instance of Item.
      * @param {string} id Item ID
@@ -55,7 +55,7 @@ export class Item extends ItemInfo {
 /**
  * Represents the login or registration response for the Awesome Store API Login / Registration route.
  */
-export class AuthResponse {
+class AuthResponse {
     /**
      * Creates a new instance of AuthResponse.
      * @param {boolean} success True if login / registration was successful. False otherwise.
@@ -68,10 +68,48 @@ export class AuthResponse {
 }
 
 /**
+ * Represents the data associated with a user (excluding their password).
+ */
+class User {
+    /**
+     * Creates a new instance of User.
+     * @param {string} first_name First name of the user.
+     * @param {string} last_name Last name of the user.
+     * @param {string} username Username of the user.
+     * @param {string} email Email of the user.
+     */
+    constructor(first_name, last_name, username, email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.email = email;
+    }
+}
+
+/**
+ * Represents the location data associated with a user.
+ */
+class UserLocation {
+    /**
+     * Creates a new instance of UserLocation.
+     * @param {string} username Username of the associated user.
+     * @param {number} latitude Longitude of the user.
+     * @param {number} longitude Longitude of the user.
+     * @param {number} timestamp Unix Timestamp in milliseconds when the location was recorded.
+     */
+    constructor(username, latitude, longitude, timestamp) {
+        this.username = username;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timestamp = timestamp;
+    }
+}
+
+/**
  * Calls route for getting all categories.
  * @returns {Promise<Array<string>>} An array of categories.
  */
-export async function get_categories() {
+async function get_categories() {
     const path = "categories";
     let categories = [];
 
@@ -94,7 +132,7 @@ export async function get_categories() {
  * parameters.
  * @returns {Promise<Array<Item>>}} An array of items matching the query.
  */
-export async function search_items({ id = null, name = null, prod_url = null, img_url = null, price = null, desc = null, category = null, tags = null, skip = null, limit = null } = {}) {
+async function search_items({ id = null, name = null, prod_url = null, img_url = null, price = null, desc = null, category = null, tags = null, skip = null, limit = null } = {}) {
     const query_params = {}
 
     // Include arguments if they are not null and are defined
@@ -158,7 +196,7 @@ export async function search_items({ id = null, name = null, prod_url = null, im
  * @param {ItemInfo} item An instance of an Item.
  * @returns {Promise<object>} JSON information with operation results.
  */
-export async function post_item(item) {
+async function post_item(item) {
     const path = "items";
     const response = await fetch(api_url + path, {
         method: "POST", headers: {
@@ -183,7 +221,7 @@ export async function post_item(item) {
  * @param {ItemInfo} item Information of an item to be updated.
  * @returns {Promise<object} JSON response indicating result of the operation.
  */
-export async function put_item(id, item) {
+async function put_item(id, item) {
     const path = "items/id/" + id;
     const response = await fetch(api_url + path, {
         method: "PUT", headers: {
@@ -209,7 +247,7 @@ export async function put_item(id, item) {
  * @param {string} id Item ID
  * @returns {Promise<object>} JSON info with results.
  */
-export async function delete_item(id) {
+async function delete_item(id) {
     const path = "items/id/" + id;
 
     const response = await fetch(api_url + path, {
@@ -234,7 +272,7 @@ export async function delete_item(id) {
  * @param {string} password Password of user
  * @returns JSON info with the results of the operation.
  */
-export async function login(username, password) {
+async function login(username, password) {
     const path = "login"
 
     const body = {
@@ -272,7 +310,7 @@ export async function login(username, password) {
  * @param {string} password User password
  * @returns JSON info with results of the operation.
  */
-export async function register(first_name, last_name, username, email, password) {
+async function register(first_name, last_name, username, email, password) {
     const path = "register"
 
     const user = {
@@ -309,7 +347,7 @@ export async function register(first_name, last_name, username, email, password)
  * @param {string} category Category name 
  * @returns {Promise<Array<Item>>} An array of items
  */
-export async function items_by_category(category, skip = 0, limit = 0) {
+async function items_by_category(category, skip = 0, limit = 0) {
     const path = `items/category/${category}?` + new URLSearchParams({ skip: skip, limit: limit })
 
     let items = [];
@@ -339,7 +377,7 @@ export async function items_by_category(category, skip = 0, limit = 0) {
  * @param {string} id Item ID
  * @returns {Promise<Item>} An item.
  */
-export async function item_by_id(id) {
+async function item_by_id(id) {
     const path = "items/id/" + id;
     let item = {}
 
@@ -363,7 +401,7 @@ export async function item_by_id(id) {
  * @param {string} id Item ID
  * @returns {Promise<string>} An image as an object URL created from a BLOB.
  */
-export async function image_by_id(id) {
+async function image_by_id(id) {
     const path = "image/id/" + id;
 
     const response = await fetch(api_url + path, { method: "GET" });
@@ -379,52 +417,145 @@ export async function image_by_id(id) {
     return URL.createObjectURL(blob);
 }
 
-// async function main() {
-//     let data;
-//     data = await get_categories();
-//     console.log("GET /categories");
-//     console.log(data);
+/**
+ * Retreives all user data.
+ * @returns {Promise<Array<User>>} An array of user data.
+ */
+async function get_all_users() {
+    const path = "users";
 
-//     data = await search_items({ limit: 2 });
-//     console.log("GET /items");
-//     console.log(data);
+    let users = [];
 
-//     data = await item_by_id("66048202614bad457356af70");
-//     console.log("GET /items/id");
-//     console.log(data);
+    const response = await fetch(api_url + path, { method: "GET" });
+    const json = await response.json();
+    users = await json["users"];
+    const status = response.status;
+    const success = status == 200;
 
-//     data = await items_by_category("Grocery & Gourmet Food", 0, 1);
-//     console.log("GET /items/category");
-//     console.log(data);
+    if (!success) {
+        throw new Error(`${status} Error getting user data.`);
+    }
+    let formatted_users = []
 
-//     data = await post_item(new ItemInfo("Name", "prod_url", "img_url", "description", 1, "Miscellaneous", ["Fortnite"]));
-//     console.log("POST /items");
-//     console.log(data);
+    // Convert response to array of Item Objects
+    users.forEach(user => {
+        formatted_users.push(new User(user.first_name, user.last_name, user.username, user.email))
+    });
 
-//     data = await put_item("65ff48a4aa724129f8404a1b", new ItemInfo("Name", "prod_url", "img_url", "description", 1, "Miscellaneous", ["Fortnite"]));
-//     console.log("PUT /items/id");
-//     console.log(data);
+    return formatted_users;
+}
 
-//     data = await delete_item("65ff48a4aa724129f8404a1b");
-//     console.log("DELETE /items");
-//     console.log(data);
+/**
+ * Retreives the associated username's user data.
+ * @param {string} username Username of the user.
+ * @returns {Promise<User>} User data.
+ */
+async function get_user_by_username(username){
+    const path = "users/username/" + username;
+    let user = {}
 
-//     data = await login("It-Is-Legend27", "thehonoredone");
-//     console.log("POST /login");
-//     console.log(data);
+    const response = await fetch(api_url + path, { method: "GET" });
+    const json = await response.json();
+    user = await json["user"];
+    const status = response.status;
+    const success = status == 200;
 
-//     data = await register("Angel", "Badillo", "FortniteMaster", "fortnite@mail.com", "fortnite");
-//     console.log("POST /register");
-//     console.log(data);
+    if (!success) {
+        throw new Error(`${status} Error getting user data.`);
+    }
 
-//     data = await item_by_id("66048202614bad457356af70");
-//     console.log("GET /item/id");
-//     console.log(data);
+    // Return user data
+    return new User(user.first_name, user.last_name, user.username, user.email);
+}
 
-//     data = await image_by_id("66048202614bad457356af70");
-//     console.log("GET /image/id");
-//     file = new File([data], "img.jpg");
-//     console.log(data);
-// }
+/**
+ * Retreives all users' location data.
+ * @returns {Promise<Array<UserLocation>>} An array of user locations.
+ */
+async function get_all_locations(){
+    const path = "locations";
 
-// main();
+    let locations = [];
+
+    const response = await fetch(api_url + path, { method: "GET" });
+    const json = await response.json();
+    locations = await json["locations"];
+    const status = response.status;
+    const success = status == 200;
+
+    if (!success) {
+        throw new Error(`${status} Error getting location data.`);
+    }
+    let formatted_locations = []
+
+    // Convert response to array of Item Objects
+    locations.forEach(location => {
+        formatted_locations.push(new UserLocation(location.username, location.latitude, location.longitude, location.timestamp))
+    });
+
+    return formatted_locations;
+}
+
+/* async function main() {
+    let data;
+    data = await get_categories();
+    console.log("GET /categories");
+    console.log(data);
+
+    data = await search_items({ limit: 2 });
+    console.log("GET /items");
+    console.log(data);
+
+    data = await item_by_id("66048202614bad457356af70");
+    console.log("GET /items/id");
+    console.log(data);
+
+    data = await items_by_category("Grocery & Gourmet Food", 0, 1);
+    console.log("GET /items/category");
+    console.log(data);
+
+    data = await post_item(new ItemInfo("Name", "prod_url", "img_url", "description", 1, "Miscellaneous", ["Fortnite"]));
+    console.log("POST /items");
+    console.log(data);
+
+    data = await put_item("65ff48a4aa724129f8404a1b", new ItemInfo("Name", "prod_url", "img_url", "description", 1, "Miscellaneous", ["Fortnite"]));
+    console.log("PUT /items/id");
+    console.log(data);
+
+    data = await delete_item("65ff48a4aa724129f8404a1b");
+    console.log("DELETE /items");
+    console.log(data);
+
+    data = await login("It-Is-Legend27", "thehonoredone");
+    console.log("POST /login");
+    console.log(data);
+
+    data = await register("Angel", "Badillo", "FortniteMaster", "fortnite@mail.com", "fortnite");
+    console.log("POST /register");
+    console.log(data);
+
+    data = await item_by_id("66048202614bad457356af70");
+    console.log("GET /item/id");
+    console.log(data);
+
+    data = await image_by_id("66048202614bad457356af70");
+    console.log("GET /image/id");
+    file = new File([data], "img.jpg");
+    console.log(data);
+}
+
+main(); */
+
+async function test_routes() {
+    let data;
+    data = await get_all_users();
+    console.log(data)
+
+    data = await get_user_by_username("It-Is-Legend27");
+    console.log(data);
+
+    data = await get_all_locations();
+    console.log(data);
+}
+
+test_routes();
